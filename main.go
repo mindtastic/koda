6e63a3f31fefa453c09fa8fa0bf4f2e207fe6e2e
Leonard Becker <10501668+ldb@users.noreply.github.com>
2022-06-01 22:05:22 +0200

    Replace UUID validation and generation

--- main.go
+++ main.go
@@ -7,15 +7,14 @@ import (
 	"net/http"
 	"sync"
 
+	"github.com/hashicorp/go-uuid"
 	"github.com/mindtastic/koda/log"
 )
 
-var port = flag.Int("port", 8000, "Port to listen on for API connections")
+var addr = flag.String("addr", ":8000", "Address to listen on for API connections")
 
 // AccountKey is a 128 bit value string used to identify users
 type AccountKey string
-
-const accountKeyLen = 2*16 + 4
 
 // Record stores multiple user ids
 type Record struct {
@@ -39,7 +38,7 @@ func main() {
 	app = &Application{
 		db: map[AccountKey]Record{},
 		httpServer: &http.Server{
-			Addr:    fmt.Sprintf(":%v", *port),
+			Addr:    *addr,
 			Handler: validateRequest(handleRequest()),
 		},
 	}
@@ -53,6 +52,7 @@ type ctxKey string
 const (
 	accountKeyHeader = "X-AccountKey"
 	serviceHeader    = "X-ForService"
+	userIDHeader     = "X-User-ID"
 
 	accountKeyCtxKey ctxKey = "accountkey"
 	serviceCtxKey    ctxKey = "service"
@@ -60,32 +60,41 @@ const (
 
 func handleRequest() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		accountKey := r.Context().Value(accountKeyCtxKey).(AccountKey)
+		accountKey, ok := r.Context().Value(accountKeyCtxKey).(AccountKey)
+		if !ok {
+			// Handle me
+		}
 
-		record, found := app.db[accountKey]
-		if !found {
+		app.mux.Lock()
+		record, ok := app.db[accountKey]
+		if !ok {
 			record = Record{
 				serviceKeys: make(map[string]string),
 			}
 			app.db[accountKey] = record
 		}
+		app.mux.Unlock()
 
-		service := r.Context().Value(serviceCtxKey).(string)
-		serviceUserId, found := record.serviceKeys[service]
-		if !found {
-			serviceUserId = randomUserId()
+		service, ok := r.Context().Value(serviceCtxKey).(string)
+		if !ok {
+
+		}
+		serviceUserId, ok := record.serviceKeys[service]
+		if !ok {
+			id, err := uuid.GenerateUUID()
+			if err != nil {
+				//idk
+			}
+			app.mux.Lock()
+			serviceUserId = id
 			record.serviceKeys[service] = serviceUserId
 			app.db[accountKey] = record
+			app.mux.Unlock()
 		}
 
-		w.Header().Set("Authentication", fmt.Sprintf("Bearer %s", serviceUserId))
+		w.Header().Set(userIDHeader, fmt.Sprintf("Bearer %s", serviceUserId))
 		w.WriteHeader(http.StatusOK)
 	}
-}
-
-func randomUserId() string {
-	panic("Not implemented")
-	return ""
 }
 
 func validateRequest(next http.HandlerFunc) http.HandlerFunc {
@@ -96,17 +105,8 @@ func validateRequest(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		if len(accountKey) != accountKeyLen {
-			log.Debugf("account key string '%v' rejected for invalid length", accountKey)
-			http.Error(w, "account key string is wrong length", http.StatusBadRequest)
-			return
-		}
-
-		if accountKey[8] != '-' ||
-			accountKey[13] != '-' ||
-			accountKey[18] != '-' ||
-			accountKey[23] != '-' {
-			log.Debugf("account key string '%v' rejected for invalid format", accountKey)
+		_, err := uuid.ParseUUID(accountKey)
+		if err != nil {
 			http.Error(w, "account key is improperly formatted", http.StatusBadRequest)
 			return
 		}
