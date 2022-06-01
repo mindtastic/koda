6dc8a3d60702547ea503dca7c3e2c7dad4b585be
Johannes Gräger <jgraeger@posteo.de>
2022-06-01 18:10:31 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mindtastic/koda
+
+go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/mindtastic/koda/log"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var port = flag.Int("port", 8000, "Port to listen on for API connections")
+
+// AccountKey is a 128 bit value string used to identify users
+type AccountKey string
+
+// Record stores multiple user ids
+type Record struct {
+	userService  string
+	wikiService  string
+	moodDiary    string
+	motivator    string
+	notifcations string
+}
+
+type Application struct {
+	mux        sync.RWMutex
+	db         map[AccountKey]Record
+	httpServer *http.Server
+}
+
+var app *Application
+
+func main() {
+	flag.Parse()
+
+	router := httprouter.New()
+	// GET /{account_key}			<--- Specific services fetches its user_id for a given account key
+	router.GET("/:account_key", FetchUserId)
+	// POST /{account_key}			<--- Creates random user ids for a given account_key
+	router.POST("/:account_key", CreateId)
+	// Later:
+	// PUT /{account_key}/rotate	<--- Rotates keys for a given ide
+
+	app = &Application{
+		db: map[AccountKey]Record{},
+		httpServer: &http.Server{
+			Addr:    fmt.Sprintf(":%v", port),
+			Handler: router,
+		},
+	}
+
+	log.Fatal(app.httpServer.ListenAndServe())
+}
+
+func FetchUserId(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func CreateId(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	accountKey := AccountKey(ps.ByName("account_key"))
+	// record := createRandomRecord()
+
+	app.mux.Lock()
+	defer app.mux.Unlock()
+
+	_, exists := app.db[accountKey]
+	if exists {
+		log.Warnf("Host %v requested id creation for known account_key: %v", r.Host, accountKey)
+		http.Error(w, "", http.StatusConflict)
+		return
+	}
+
+}
